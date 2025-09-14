9e7505e3463e724af6f03028d4204393464d7bab
Sammyverse0 <bearkills27@gmail.com>
2025-09-14 22:43:55 +0530

    Added the paddy situation box
    When player reaches 150 GI they get a situation box where, after some explaination the player is asked to choose b/w two options

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,6 +1,19 @@
+// Building.cs
 using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    public int ecoGain = 2; // set in inspector
+    public int ecoGain = 2; // how much GI this building gives per second
+}
+
+// 🆕 Special lightweight building (not a MonoBehaviour)
+[System.Serializable]
+public class VirtualBuilding
+{
+    public int ecoGain;
+
+    public VirtualBuilding(int gain)
+    {
+        ecoGain = gain;
+    }
 }
